01add0a781a34db8e1c7f28c5c3ed4e0150dc1e2
Aaditya Shah <0blvn@protonmail.com>
2024-10-23 23:34:11 +0530

    Dummy commit

--- setup.py
+++ setup.py
@@ -13,3 +13,4 @@ def _create_index_directory_if_not_exists(path="./index"):
 if __name__ == "__main__":
     _create_storage_directory_if_not_exists()
     _create_index_directory_if_not_exists()
+    
